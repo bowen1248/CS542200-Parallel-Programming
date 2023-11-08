339b2c82746c9d982d2e5c411ac829adea098926
Bowen1248 <bowenlee31@gmail.com>
2023-11-08 11:00:47 +0800

    Add hw2

--- hw1/main/hw1_mpi.sh
+++ hw1/main/hw1_mpi.sh
@@ -11,4 +11,4 @@ export LD_PRELOAD=/opt/ipm/lib/libipm.so
 export IPM_HPM=“PAPI_TOT_INS,PAPI_TOT_CYC,PAPI_REF_CYC,PAPI_SP_OPS,PAPI_DP_OPS,PAPI_VEC_SP,PAPI_VEC_DP”
 # mpirun ./hw1 65536 ../testcases/03.in ./03.out
 # mpicc -n 12 ./hw1 536869888 ../testcases/40.in ./40.out
-IPM_REPORT=full IPM_REPORT_MEM=yes IPM_LOG=full LD_PRELOAD=/opt/ipm/lib/libipm.so srun -p judge -N1 -n12 ./hw1 536869888 ../testcases/40.in ./40.out
+IPM_REPORT=full IPM_REPORT_MEM=yes IPM_LOG=full LD_PRELOAD=/opt/ipm/lib/libipm.so srun -p judge -N3 -n12 ./hw1 536869888 ../testcases/40.in ./40.out
